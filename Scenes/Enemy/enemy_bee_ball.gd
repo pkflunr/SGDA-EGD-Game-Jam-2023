f2deb480ff048048a7266536e876928f11508fde
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-04-21 20:03:48 -0700

    create bee ball enemy

---
+++ Scenes/Enemy/enemy_bee_ball.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var aggro_target:Node2D
+@export var acceleration = 650 # in pixels/sec/sec
+@export var terminal_velocity = 400
+
+var velocity:Vector2 = Vector2.ZERO
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if aggro_target != null:
+		velocity += position.direction_to(aggro_target.position) * acceleration * delta
+		velocity = velocity.limit_length(terminal_velocity)
+		position += velocity * delta
