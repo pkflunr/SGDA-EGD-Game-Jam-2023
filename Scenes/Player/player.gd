9d6e5bb184ea6c744fe66916fc2001e28a17c9af
anvilsp <77859117+anvilsp@users.noreply.github.com>
2023-04-21 22:50:51 -0700

    Basic shooting

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-# CONSTANTS
+# ---CONSTANTS---
 
 # movement
 const ACCELERATION = 30
@@ -10,7 +10,7 @@ const AIR_FRICTION = 0.025
 # player
 var DEFAULT_DRAIN = 1.0
 
-# VARIABLES
+# ---VARIABLES---
 
 # movement
 var direction = 1 # 1 is right, -1 is left
@@ -26,38 +26,44 @@ func _physics_process(delta):
 	var input_x = Input.get_axis("player_left", "player_right")
 	var input_y = Input.get_axis("player_up", "player_down")
 
-	if input_x != 0:
+	if input_x != 0: # horizontal movement
 		velocity.x += input_x * ACCELERATION
 	else:
+		# taper off speed if there is no input
 		velocity.x = lerpf(velocity.x, 0, AIR_FRICTION) if abs(velocity.x) > 5 else 0
-	if input_y != 0:
+	if input_y != 0: # vertical movement
 		velocity.y += input_y * ACCELERATION
 	else:
 		velocity.y = lerpf(velocity.y, 0, AIR_FRICTION) if abs(velocity.y) > 5 else 0
 	
-	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-	velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
+	# Clamp movement to maximum speed
+	velocity = Vector2(clamp(velocity.x, -MAX_SPEED, MAX_SPEED), clamp(velocity.y, -MAX_SPEED, MAX_SPEED))
 	
 	# this is VERY temporary
-	if Input.is_action_just_pressed("player_left"):
+	if Input.is_action_pressed("player_left"):
 		direction = -1
 		$Sprite.scale.x = -0.688
-	if Input.is_action_just_pressed("player_right"):
+	if Input.is_action_pressed("player_right"):
 		direction = 1
 		$Sprite.scale.x = 0.688
 	
-	debug_label.set_text("Speed: (%f, %f)\nDirection: %d\nHealth: %d\nDrain Rate: %d/sec" % [velocity.x, velocity.y, direction, health, drain_rate])
-	
 	move_and_slide()
-
-	if health <= 0:
+	
+	# Check if health has drained
+	if health <= 0 or Input.is_action_pressed("restart"):
 		die()
+	
+	debug_label.set_text("Speed: (%f, %f)\nDirection: %d\nHealth: %d\nDrain Rate: %d/sec" % [velocity.x, velocity.y, direction, health, drain_rate])
 
-func die():
+func hurt(damage_value : int):
+	# take a set amount of damage
+	health -= damage_value
+	if health < 0:
+		health = 0
+
+func die(): # the bee is dead
 	get_tree().reload_current_scene()
 
-func _on_timer_timeout():
-	if health > 0:
-		health -= drain_rate
-		if health < 0:
-			health = 0
+func _on_drain_timer_timeout():
+	# Health drain timer
+	hurt(drain_rate)
