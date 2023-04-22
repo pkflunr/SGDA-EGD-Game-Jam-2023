c76d1258778e532f1e64b0ca57816543487991b9
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-04-21 23:30:49 -0700

    update bee ball animation

--- Scenes/Enemy/enemy_bee_ball.gd
+++ Scenes/Enemy/enemy_bee_ball.gd
@@ -4,6 +4,9 @@ extends Area2D
 @export var acceleration = 640 # in pixels/sec/sec
 @export var terminal_velocity = 400
 
+@onready var gpu_particles_2d = $GPUParticles2D
+
+
 var velocity:Vector2 = Vector2.ZERO
 
 func _ready():
@@ -16,3 +19,5 @@ func _physics_process(delta):
 		velocity += position.direction_to(aggro_target.position) * acceleration * delta
 		velocity = velocity.limit_length(terminal_velocity)
 		position += velocity * delta
+	
+	
