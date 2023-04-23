f1b8845faa86daeb638200381e95523cbace0c1b
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2023-04-22 17:07:57 -0700

    shooters -> guns to reduce confusion, also they get their own folder

--- Scenes/Enemy/shooters/enemyBasicGun.gd
+++ Scenes/Enemy/shooters/enemyBasicGun.gd
@@ -11,12 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_pressed("player_shoot"):
-		fire()
+	pass
 
-func fire():
+func fire(direction : Vector2):
 	if(!cooldown_timer.time_left):
-		var direction = Vector2(get_parent().direction, 0)
 		var b = bullet.instantiate()
 		b.direction = direction
 		var b2 = Area2D.new()
--- Scenes/Objects/bullet.gd
+++ Scenes/Objects/bullet.gd
@@ -4,13 +4,17 @@ extends Area2D
 @export var projectile_lifetime = 0.5
 @export var damage = 5
 var direction = Vector2(1, 0)
+var fading = false
 
 func _ready():
 	$Lifetime.wait_time = projectile_lifetime
 	$Lifetime.start()
 
 func _physics_process(delta):
-	position = position + speed * direction * delta	
+	if !fading and $Lifetime.time_left < 0.25 :
+		fading = true
+		$AnimationPlayer.play("fade")
+	position = position + speed * direction * delta
 
 func _on_lifetime_timeout():
 	self.queue_free()
