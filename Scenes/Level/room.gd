6dbf82ba40194200b667c8d6e7529dc0d60401b9
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-04-23 00:02:28 -0700

    add areas to rooms for aggroing enemies

--- Scenes/Level/room.gd
+++ Scenes/Level/room.gd
@@ -5,6 +5,8 @@ class_name Room
 enum {TOP,BOTTOM,LEFT,RIGHT}
 @onready var tile_map = $TileMap
 
+var enemies_in_room:Array = []
+
 @export_flags("TOP","BOTTOM","LEFT","RIGHT") var active_walls = 0
 @export var marked = false
 
@@ -14,10 +16,14 @@ const BOTTOM_LEFT_ID = 0
 const BOTTOM_RIGHT_ID = 1
 const UPPER_RIGHT_ID = 3
 const UPPER_LEFT_ID = 4
+@onready var room_area = $RoomArea
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if room_area != null:
+		for body in room_area.get_overlapping_bodies():
+			if body.is_in_group("enemies"):
+				enemies_in_room.append(body)
 
 func _process(delta):
 	if (active_walls & (1 << TOP)):
@@ -44,3 +50,15 @@ func _process(delta):
 	else:
 		for i in range(12,16):
 			tile_map.set_cell(0,Vector2i(27,i))
+
+func _on_room_area_body_entered(body):
+	if body.is_in_group("player"):
+		for enemy in enemies_in_room:
+			if "aggro_target" in enemy:
+				enemy.aggro_target = body
+			if "player" in enemy:
+				enemy.player = body
+	elif body.is_in_group("enemy"):
+		if not enemies_in_room.has(body):
+			print("room made a friend")
+			enemies_in_room.append(body)
