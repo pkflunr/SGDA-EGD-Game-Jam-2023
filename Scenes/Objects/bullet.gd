b263e15721009851ffd7b29eb33bf7b80821ed20
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2023-04-22 20:13:14 -0700

    player bullet fades out and lasts longer

--- Scenes/Objects/bullet.gd
+++ Scenes/Objects/bullet.gd
@@ -1,15 +1,19 @@
 extends Area2D
 
 @export var speed = 1000
-@export var projectile_lifetime = 0.5
+@export var projectile_lifetime = 0.75
 @export var damage = 5
 var direction = Vector2(1, 0)
+var fading = false
 
 func _ready():
 	$Lifetime.wait_time = projectile_lifetime
 	$Lifetime.start()
 
 func _physics_process(delta):
+	if !fading and $Lifetime.time_left < 0.25 :
+		fading = true
+		$AnimationPlayer.play("fade")
 	position = position + speed * direction * delta	
 
 func _on_lifetime_timeout():
