443dad8d67f68f8ba6dcc298417d4e957427af94
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2023-04-23 15:57:23 -0700

    Revert "Merge branch 'safety-branch' into waltenv2"
    This reverts commit 75ca96de86d29455466ad3b103ea55257ab289f6, reversing
    changes made to 7b5bca4d371c8ea5ae89034a813f9d60350a96b3.

--- Scenes/UI/pause_menu.gd
+++ Scenes/UI/pause_menu.gd
@@ -34,7 +34,6 @@ func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
 func _on_exit_pressed():
-	Globals.end_game()
 	set_paused(false)
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
 
