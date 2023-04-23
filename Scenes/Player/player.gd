c2b12184d330414253226e77e4048b77b876fef5
anvilsp <77859117+anvilsp@users.noreply.github.com>
2023-04-23 15:45:04 -0700

    End screen stuff

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -37,6 +37,9 @@ var pause_cooldown = false # so fucking tired of htis shit
 
 @onready var curr_gun = $Shooter
 
+func _ready():
+	Globals.start_game()
+
 func _physics_process(delta):
 	# Movement stuff
 	input_x = Input.get_axis("player_left", "player_right")
@@ -120,7 +123,7 @@ func die(): # the bee is dead
 			$DeathParticle.emitting = true
 
 func exit():
-#	Globals.end_game()
+	Globals.end_game()
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
 
 func switch_gun(gun : PackedScene):
@@ -150,6 +153,7 @@ func _on_dash_hurtbox_body_entered(body):
 			set_health(body.health_when_possessed)
 			if "take_over_gun" in body:
 				switch_gun(body.take_over_gun)
+			Globals.enemies_captured += 1
 		else:
 			die()
 
