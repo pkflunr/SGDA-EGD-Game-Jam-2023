44530260eb448b2eeb97b75be501e2be415644a4
anvilsp <77859117+anvilsp@users.noreply.github.com>
2023-04-23 17:52:31 -0700

    Merge branch 'safety-branch' into anvil-safety

--- Scenes/UI/win_menu.gd
+++ Scenes/UI/win_menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
-
-
+func _ready():
+	$CenterContainer/VBoxContainer/BeesContainer/Bees.set_text("%d" % Globals.enemies_captured)
+	$CenterContainer/VBoxContainer/TimeContainer/Time.set_text("%s" % Globals.convert_time(Globals.global_timer))
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
+
+
+func _on_button_pressed_2():
+	get_tree().change_scene_to_file("res://Scenes/Level/level.tscn")
